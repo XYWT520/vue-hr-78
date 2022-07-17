a21c33eab1a4cd49b4adead9001ecec39c6fed0b
ZYC <2095921712@qq.com>
2022-07-17 21:46:23 +0800

    feat:公司设置管理

---
+++ src/api/setting.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+/**
+ * @description: 获取下拉员工数据
+ * @param {*}
+ * @return {*}
+ */
+export function getRoles(params) {
+  return request({
+    url: '/sys/role',
+    params
+  })
+}
--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,10 +1,60 @@
 <template>
-  <div class="department-container">
+  <div class="dashboard-container">
     <div class="app-container">
       <el-card>
-        <!-- 具体页面结构 -->
-        公司设置
+        <el-tabs>
+          <!-- 放置页签 -->
+          <el-tab-pane label="角色管理">
+            <!-- 新增角色按钮 -->
+            <el-row style="height:60px">
+              <el-button
+                icon="el-icon-plus"
+                size="small"
+                type="primary"
+              >新增角色</el-button>
+            </el-row>
+            <!-- 表格 -->
+            <el-table :data="rolesList">
+              <el-table-column type="index" label="序号" width="120" />
+              <el-table-column prop="name" label="角色名称" width="240" />
+              <el-table-column prop="description" label="描述" />
+              <el-table-column label="操作">
+                <template slot-scope="scope">
+                  <el-button size="small" type="success">分配权限</el-button>
+                  <el-button size="small" type="primary">编辑</el-button>
+                  <el-button size="small" type="danger">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+
+            <el-row type="flex" justify="center" align="middle" style="height: 60px">
+              <!-- 分页组件 -->
+              <el-pagination layout="prev,pager,next" />
+            </el-row>
+          </el-tab-pane>
+        </el-tabs>
       </el-card>
     </div>
   </div>
 </template>
+
+<script>
+import { getRoles } from '@/api/setting'
+export default {
+  data() {
+    return {
+      rolesList: []
+    }
+  },
+  created() {
+    this.loadGetRoles()
+  },
+
+  methods: {
+    async loadGetRoles() {
+      const res = await getRoles()
+      this.rolesList = res.data.rows
+    }
+  }
+}
+</script>
